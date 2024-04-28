a6b1134262e06c87c1f0b7e4706b1e7c2d0edef8
Bouheni Mohamed <mohamed.bouheni@esprit.tn>
2024-04-28 14:13:45 +0100

    change

--- src/app/BackOffice/all-template-back/all-template-back.component.ts
+++ src/app/BackOffice/all-template-back/all-template-back.component.ts
@@ -4,16 +4,13 @@ import { GoogleAiService } from 'src/app/google-ai.service';
 @Component({
   selector: 'app-all-template-back',
   templateUrl: './all-template-back.component.html',
-  styleUrls: ['./all-template-back.component.css']
+  styleUrls: ['./all-template-back.component.css'],
 })
 export class AllTemplateBackComponent {
-  constructor( private gaservice : GoogleAiService){
-
-  }
-  ngOnInit(){
-    this.gaservice.generateStory("write me a code for hello world with python").subscribe(
-      (res) => console.log(res.candidates[0].content.parts[0].text)
-    )
-
+  constructor(private gaservice: GoogleAiService) {}
+  ngOnInit() {
+    this.gaservice
+      .generateStory('write me a code for hello world with python')
+      .subscribe((res) => console.log(res.candidates[0].content.parts[0].text));
   }
 }
